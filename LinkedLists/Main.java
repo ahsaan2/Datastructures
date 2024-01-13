1c2bd761cf275622bdcfda23799bd3e58a451ddb
Ahsaan <ahsaan1345@outlook.com>
2024-01-13 11:54:59 +0530

    Added Add functionality to LinkedList

---
+++ LinkedLists/Main.java
@@ -0,0 +1,17 @@
+package LinkedLists;
+
+public class Main {
+    public static void main(String[] args) {
+        SingleLinkedList list = new SingleLinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.print();
+        /*
+         * 
+         * Properties of list
+         * i should be able to Add new elements
+         * i should be able to delete element at any index 
+         */
+    }
+}
--- LinkedLists/SingleLinkedList.java
+++ LinkedLists/SingleLinkedList.java
@@ -1,4 +1,38 @@
 
-public class SingleLinkedList{
+package LinkedLists;
 
+class LinkedListNode {
+    int value;
+    LinkedListNode next;
+
+    public LinkedListNode(int value) {
+        this.value = value;
+    }
+}
+
+/**
+ * SingleLinkedList
+ */
+public class SingleLinkedList {
+    LinkedListNode tail; // points to the last element in the linkedList 
+    LinkedListNode head; // points to the first node in the linkedlist;
+
+    public void add(int value) {
+        LinkedListNode node = new LinkedListNode(value);
+        if (this.tail == null || this.head == null) {
+            this.tail = node;
+            this.head = node; // this is wrong
+        } else {
+            tail.next = node;
+            this.tail = tail.next; // we update tail to point to the new element added in the linkedlist
+        }
+    }
+
+    public void print(){
+        LinkedListNode tempHead = head;
+        while(tempHead != null){
+            System.out.println(tempHead.value);
+            tempHead = tempHead.next;
+        }
+    }
 }
