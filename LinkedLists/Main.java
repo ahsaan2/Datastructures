0b92824e20daddd7592423abd4b9a48a6dcfad7c
Ahsaan <ahsaan1345@outlook.com>
2024-01-13 18:17:03 +0530

    CustomLinkedListInsertions

--- LinkedLists/Main.java
+++ LinkedLists/Main.java
@@ -2,11 +2,43 @@ package LinkedLists;
 
 public class Main {
     public static void main(String[] args) {
-        SingleLinkedList list = new SingleLinkedList();
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.print();
+        //SingleLinkedList list = new SingleLinkedList();
+        try{
+                CustomLinkedList list1 = new CustomLinkedList();
+
+                // CustomLinkedList<String> strList = new CustomLinkedList<>();
+                // strList.insert("ab");
+                // strList.insert("bc");
+                // strList.insert("cd");
+                // strList.insert("de");
+                // strList.display();
+
+                list1.insert(12); 
+                list1.insert(21);
+                list1.insert(22);
+                //  // 22 21 12 
+                // list1.insertLast(30);
+                // list1.insertLast(40);
+                list1.insertAtIndex(90, 0);
+                list1.insert(10);
+                list1.display(); // 12, 21, 22, 30 , 40
+                System.out.println(list1.size());
+        }catch(Exception e){
+                System.out.println(e.getMessage());
+        }
+
+
+        // list.add(1);
+        // System.out.println("Size "+list.size());
+        // list.add(2);
+        //         System.out.println("Size "+list.size());
+
+        // list.add(3);
+        //         System.out.println("Size "+list.size());
+            
+
+        // list.contains(3);
+        // list.print();
         /*
          * 
          * Properties of list
--- LinkedLists/SingleLinkedList.java
+++ LinkedLists/SingleLinkedList.java
@@ -14,10 +14,11 @@ class LinkedListNode {
  * SingleLinkedList
  */
 public class SingleLinkedList {
-    LinkedListNode tail; // points to the last element in the linkedList 
+    LinkedListNode tail; // points to the last element in the linkedList
     LinkedListNode head; // points to the first node in the linkedlist;
+    private int size = 0;
 
-    public void add(int value) {
+    public boolean add(int value) {
         LinkedListNode node = new LinkedListNode(value);
         if (this.tail == null || this.head == null) {
             this.tail = node;
@@ -26,11 +27,82 @@ public class SingleLinkedList {
             tail.next = node;
             this.tail = tail.next; // we update tail to point to the new element added in the linkedlist
         }
+        size += 1;
+        return true;
     }
 
-    public void print(){
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return this.head == null;
+    }
+    
+
+
+    public boolean contains(int value) {    // we have to traverse the linked list to find out if the value we are looking for is present or not
+        
         LinkedListNode tempHead = head;
+        while (tempHead != null) {
+            if (tempHead.value == value)
+                return true;
+                // 5 -- 7 -- 9 -- 10
+            tempHead = tempHead.next;
+        }
+        return false;
+    }
+
+    public boolean remove(int value){
+        // remove the node having value equals to value;
+        if(this.head == null){
+            return false;
+        }
+        
+            // If head and tail points to the same element that is we have only one element in the linkedList
+        if(this.head == this.tail && this.head.value == value){
+            // once we remove the head --> tail also becomes null
+            // we are having only single element in linkedList;
+            this.head = null;
+            this.tail = null;
+            size -= 1;
+            return true;
+        }
+        // if head.value == value
+        if(this.head.value == value){
+            // we have to remove head;
+            this.head = this.head.next;
+            size -= 1;
+            return true;
+        }
+
+        LinkedListNode tempHead = head;
+        LinkedListNode prev = head;
         while(tempHead != null){
+            if(tempHead.value == value){
+                if(tempHead.next == null){
+                    // we are at tail;
+                    prev.next = null;
+                    this.tail = prev;
+                    size -= 1;
+                    return true;
+                }
+                // If we are removing in middle.
+                // we found it.
+                tempHead.next = null;
+                prev.next = tempHead.next.next;
+                size -= 1;
+                return true;
+            }
+            prev = tempHead;
+            tempHead = tempHead.next;
+        }
+        return false;
+    }
+
+    public void print() {
+        LinkedListNode tempHead = head;
+        while (tempHead != null) {
             System.out.println(tempHead.value);
             tempHead = tempHead.next;
         }
