6bc114534823f9b8cfa1f80f8ddd2ba9f61ea00e
Ahsaan <ahsaan1345@outlook.com>
2024-01-22 20:24:18 +0530

    Custom Implementation of the static Stack

---
+++ ObjectOrientedProgramming/StacksAndQueues/StackMain.java
@@ -0,0 +1,26 @@
+package ObjectOrientedProgramming.StacksAndQueues;
+
+public class StackMain {
+    public static void main(String[] args)throws Exception{
+        StackCustomImplementation stack = new StackCustomImplementation(5);
+        stack.push(23);
+        stack.push(21);
+        stack.push(24);
+        stack.push(43);
+        stack.push(21);
+        stack.push(21);  //Exception-->> can not push stack is full
+        stack.push(21);  // Exception -->> can not push stack is full
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        // System.out.println(stack.pop());
+        System.out.println("Peek item is:");
+        System.out.println(stack.peek());  // 21
+        System.out.println(stack.isEmpty());   // false
+    
+        
+    }
+   
+}
