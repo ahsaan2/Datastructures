7a8b173b3638c629c17f3bd47ea30a7066eddbbf
Ahsaan <ahsaan1345@outlook.com>
2024-03-18 20:24:13 +0530

    Missing number 2 methods

--- Java/missingNumber.java
+++ Java/missingNumber.java
@@ -12,18 +12,37 @@ public class missingNumber {
     // 20 - 7 = 13 -> 13 - 8 = 5
     // finding the missing number
     public int missingnumber(int[] arr) {
-        // our length/ total length will be arr.length +1 because of the one missing
+        // our length total length will be arr.length +1 because of the one missing
         // integer
         int n = arr.length + 1;
         // now sum is equal to
         int sum = n * (n + 1) / 2;
         for (int number : arr) {
-            // our new sum will be equal to the sum - number, and the last number sum that
+            // our new sum will be equal to the sum - number, and the last number sum
+            // that
             // we will have
             // will only be the missing number
             sum = sum - number;
         }
         return sum;
+    }
+
+    public int missing(int[] arr) {
+        // at index 0 we have 1
+        // at index 1 we have 2
+        // at index 2 we have 3
+        // at index 3 we have 5
+        // at index 4 we have 6
+        // so the missing number is 4 that is array at index != index +1
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != i + 1) {
+                return i + 1;
+
+            }
+
+        }
+        return -1;
+
     }
 
     public void display(int[] arr) {
@@ -34,9 +53,15 @@ public class missingNumber {
 
     public static void main(String[] args) {
         missingNumber obj = new missingNumber();
-        int[] arr = { 1, 2, 3, 5, 6 };
-        int ans = obj.missingnumber(arr);
-        System.out.println(ans);
+        // these two methods will give us only one missing number and if we have to find
+        // the more than one missing number, we can create a list that can hold our
+        // missing numbers
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 9 };
+        int[] arr2 = { 1, 2, 4, 5, 6, 7, 8 };
+        int ans = obj.missingnumber(arr2);
+        System.out.println(ans); // 3
+        int ans2 = obj.missing(arr);
+        System.out.println(ans2); // 2
 
     }
 
