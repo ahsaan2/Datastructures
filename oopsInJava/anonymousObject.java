1ac59d9a958d1f6535f05a8fd78cbd5da743f7c3
Ahsaan <ahsaan1345@outlook.com>
2024-03-31 16:24:27 +0530

    anonymous Objects

--- oopsInJava/anonymousObject.java
+++ oopsInJava/anonymousObject.java
@@ -2,13 +2,13 @@ package oopsInJava;
     class person{
         person(){
             super();
-            System.out.println("I am in class A");
+            System.out.println("I am in class person");
         }
     }
     class human extends person{
         human(){
             super();
-            System.out.println("I am in class B");
+            System.out.println("I am in class human");
         }        
     }
 public class anonymousObject {
@@ -16,8 +16,10 @@ public class anonymousObject {
     // anonymous objects allow you to create  an object without any name assigned to it.
 
     public static void main(String[] args) {
-        new A();  // anonymous object, will call the constructor of A
+        // new person();  // anonymous object, will call the constructor of A
         // prints I am in class a
+        new human();  // i am in class person and i am in class human
+        // every time it creates a new object and we cannot reuse it.
         
     }
 }
