a2e7bb8eedebacb55f8456a21e53386f35269704
Ahsaan <ahsaan1345@outlook.com>
2024-03-31 17:14:37 +0530

    Inheritance in java

---
+++ oopsInJava/inheritanceInJava.java
@@ -0,0 +1,51 @@
+package oopsInJava;
+
+class calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+}
+
+class newCalculator extends calculator {
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
+
+public class inheritanceInJava {
+    public static void main(String[] args) {
+        // calculator obj = new calculator();
+        // int addAns = obj.add(12, 12);
+        // System.out.println(addAns);
+        newCalculator cal = new newCalculator();
+        int ans2 = cal.divide(12, 2);
+        System.out.println(ans2);
+        int ans3 = cal.add(12, 12); // extended property
+        System.out.println(ans3);
+    }
+
+}
+// java supports single level inheritance, multi-level inheritance
+// -->> multi-level inheritance=>when we have a class that is getting inherited by something else, ans it itself is getting inherited 
+// by something else.
+// (parent(A) --> child(B)(this becomes now parent) --> child).
+// -->> Java does not support multiple inheritance.(because of the ambiguity, suppose if the two parent class has the same method
+// and we call the method. Java gets confused which method to choose.)
+// java does not support multiple inheritance
+
+/**
+ * what ever property your parents have ,its yours also
+ * Use-> if we have the same features present, and we want to use them again and
+ * agiain, we need not to add them
+ * . we can just use those features where ever we need.
+ * 
+ */
