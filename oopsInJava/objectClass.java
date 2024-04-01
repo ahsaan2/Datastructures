b441eafc2ef1c5fffba07f0c3920a0c5028b9269
Ahsaan <ahsaan1345@outlook.com>
2024-04-01 23:31:43 +0530

    Object class in java

---
+++ oopsInJava/objectClass.java
@@ -0,0 +1,37 @@
+package oopsInJava;
+
+// Every class in java extends and calls object class
+class Laptop {
+    private String model;
+    int price;
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    // creating our own toString method
+    @Override  
+    public String toString(){
+        return "hey";
+    }
+}
+
+public class objectClass {
+    public static void main(String[] args) {
+        Laptop obj = new Laptop();
+        obj.setModel("Lenovo yoga");
+        System.out.println(obj.getModel());
+
+        System.out.println(obj);  // since, it will call the super class toString() here we have created our own toString() method.
+         // will give us the package name, class name and some hashcode values.
+        // *** Every time we try to print the object , it will call the toString()
+        // method
+        System.out.println(obj.toString());  // now it will give us hey, not the hashcode
+        // toString() --> return the class name, @ and Integer.toHexString(hashCode)
+        // oopsInJava.Laptop@36baf30c
+    }
+
+}
