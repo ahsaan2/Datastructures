970fcb67bbf3bbecb1252dc470af1a5055378b3c
Ahsaan <ahsaan1345@outlook.com>
2024-04-02 23:34:18 +0530

    object class in java

--- oopsInJava/anonymousObject.java
+++ oopsInJava/anonymousObject.java
@@ -1,25 +1,32 @@
 package oopsInJava;
     class person{
         person(){
-            super();
+           super();  // every class calls the object class, and here the super class present shows that the object class is called
+            // also no need to mention super here
             System.out.println("I am in class person");
         }
     }
     class human extends person{
         human(){
             super();
+            // In inheritance what happens is that once we create the object, the construtor of the object is called
+            // but just before the constructor calling, super() class is called. and that super class takes the control to
+            // the parent class and calls the constructor of that method first.
             System.out.println("I am in class human");
         }        
     }
 public class anonymousObject {
-    // Anonymous objects-->> means nameless, We create an object without any reference variable.
+    // Anonymous objects-->> means nameless, We create an object without any "reference" variable.
     // anonymous objects allow you to create  an object without any name assigned to it.
 
     public static void main(String[] args) {
-        // new person();  // anonymous object, will call the constructor of A
-        // prints I am in class a
+        // syntax of Anonymous Object is->
+        // new className()
+        new person();  // anonymous object, will call the constructor of A
+        // prints I am in class person
         new human();  // i am in class person and i am in class human
         // every time it creates a new object and we cannot reuse it.
+        // new person();
         
     }
 }
--- oopsInJava/objectClass.java
+++ oopsInJava/objectClass.java
@@ -1,8 +1,13 @@
 package oopsInJava;
 
-// Every class in java extends and calls object class
+// Every class in java extends and calls object class.
+// All objects including the arrays, implement the methods of this class.
 class Laptop {
-    private String model;
+    private String model; // private means that the variable is visible only and only in this class
+    // cannot be accessed in the below class, so if we want to access this then we
+    // need to use the getter and setter method
+    // getter will get the variable and since we use public specifier, it will then
+    // be accessed anywhere.
     int price;
 
     public void setModel(String model) {
@@ -12,9 +17,12 @@ class Laptop {
     public String getModel() {
         return model;
     }
+
     // creating our own toString method
-    @Override  
-    public String toString(){
+    @Override // is an Annotation, used to generate the error message if the method does not
+              // override or implement the method declared
+    // in super type
+    public String toString() {   // Overrides the main toString() method.
         return "hey";
     }
 }
@@ -25,11 +33,12 @@ public class objectClass {
         obj.setModel("Lenovo yoga");
         System.out.println(obj.getModel());
 
-        System.out.println(obj);  // since, it will call the super class toString() here we have created our own toString() method.
-         // will give us the package name, class name and some hashcode values.
+        System.out.println(obj); // since, it will call the super class toString() here we have created our own
+                                 // toString() method.
+        // will give us the package name, class name and some hashcode values.
         // *** Every time we try to print the object , it will call the toString()
         // method
-        System.out.println(obj.toString());  // now it will give us hey, not the hashcode
+        System.out.println(obj.toString()); // now it will give us hey, not the hashcode
         // toString() --> return the class name, @ and Integer.toHexString(hashCode)
         // oopsInJava.Laptop@36baf30c
     }
