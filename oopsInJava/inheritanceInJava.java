0714a6f4c61500d88c063e738a29eb0f91c19e70
Ahsaan <ahsaan1345@outlook.com>
2024-04-12 13:01:45 +0530

    Inheritance in java

--- oopsInJava/inheritanceInJava.java
+++ oopsInJava/inheritanceInJava.java
@@ -1,6 +1,10 @@
 package oopsInJava;
 
 class calculator {
+    calculator(){
+       // super();  // calls the object class, we we mention super() here or not.
+        System.out.println("I am the parent class of calculator:");
+    }
     public int add(int a, int b) {
         return a + b;
     }
@@ -16,7 +20,14 @@ class calculator {
 // }
 
 class newCalculator extends calculator {
+    newCalculator(){
+        // super keyword must be always the first statement in the constructor
+        super();
+        System.out.println("I am in the child class of calculator:");
+       
+    }
     public int divide(int a, int b) {
+
         return a / b;
     }
 
@@ -31,11 +42,14 @@ public class inheritanceInJava {
         // calculator obj = new calculator();
         // int addAns = obj.add(12, 12);
         // System.out.println(addAns);
-        newCalculator cal = new newCalculator();
+        newCalculator cal = new newCalculator(); // once we create the object of the class, it will first call the constructor of that 
+        // class and because of super keyword the execution of the program will keep on going to the upper or paent class.
         int ans2 = cal.divide(12, 2);
         System.out.println(ans2);
         int ans3 = cal.add(12, 12); // extended property
         System.out.println(ans3);
+        int ans4 = cal.sub(10, 5);
+        System.out.println(ans4);
     }
 
 }
