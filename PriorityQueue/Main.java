73bfc3fd8909809c1c05eef9f7f7755ffb13016a
Ahsaan <ahsaan1345@outlook.com>
2024-07-17 00:05:32 +0530

    Priority Queue

--- PriorityQueue/Main.java
+++ PriorityQueue/Main.java
@@ -1,40 +1,80 @@
 package PriorityQueue;
 
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) throws Exception {
-        minHeap mh = new minHeap(6);
-        heaps1 hh = new heaps1(6);
-        maxHeap mx = new maxHeap(6);
+        // MinHeap mh = new MinHeap(6);
+        // heaps1 hh = new heaps1(6);
+        // maxHeap mx = new maxHeap(6);
+        MinHeap<String> strHeap = new MinHeap<String>(5,(a,b) -> {
+            if(a.length() > b.length()){
+                return 1;
+            }
+            if(a.length() < b.length()){
+                return -1;
+            }
+            // same
+            int len = a.length();
+            for(int i=0;i<len;i++){
+                char ch1 = a.charAt(i);
+                char ch2 = b.charAt(i);
+                if(ch1 == ch2) continue;
+                if(ch1 > ch2){
+                    return 1;
+                }
+                if(ch1 < ch2) {
+                    return -1;
+                }
+            }
+            return 0;
+
+        }); // This is wrong
+        strHeap.insert("Shaana");
+        strHeap.insert("Anwar");
+
+        System.out.println("-------------------------------------------");
+        MinHeap<Integer> heap = new MinHeap<Integer>(5, (a,b) -> {
+            if(a == b) return 0;
+            if(a > b) return -1;
+            return 1;
+        });
+        heap.insert(5);
+        heap.insert(6);
+        heap.insert(7);
+        heap.insert(8);
+        // heap.insert(9);
+        // heap.display();
+        while (!heap.isEmpty()) {
+            System.out.println("Polled "+heap.remove());
+        }
+        // strHeap.display();
         // mh.insert(10);
         // mh.insert(0);
         // mh.insert(5);
         // mh.insert(3);
-        mh.insert(5);
-        mh.insert(4);
-        mh.insert(3);
-        mh.insert(2);
-        mh.insert(1);
-        hh.offer(5);
+        // mh.insert(5);
+        // mh.insert(4);
+        // mh.insert(3);
+        // mh.insert(2);
+        // mh.insert(1);
+        // hh.offer(5);
 
         
-        hh.offer(4);
-        hh.offer(3);
-        hh.offer(2);
-        hh.offer(1);
-        mh.insert(1);
-        mx.insert(1);
-        mx.insert(2);
-        mx.insert(3);
-        mx.insert(4);
-        mx.insert(5);
+        // hh.offer(4);
+        // hh.offer(3);
+        // hh.offer(2);
+        // hh.offer(1);
+        // mh.insert(1);
+        // mx.insert(1);
+        // mx.insert(2);
+        // mx.insert(3);
+        // mx.insert(4);
         // mx.insert(5);
         // mx.insert(5);
+        // mx.insert(5);
 
-        
-       
-
-
-        mx.display();
+        // mx.display();
 
         // while (!mh.isEmpty()) {
         //     System.out.println("Popped = " + mh.remove());
--- PriorityQueue/minHeap.java
+++ PriorityQueue/minHeap.java
@@ -1,22 +1,24 @@
 package PriorityQueue;
 
-public class minHeap {
+import java.util.Comparator;
+
+class MinHeap<TDataType> {
     // In min-heap the root node value is smaller then its child node values.
-    int[] array;
+    Object[] array; // string, character, CustomObject -> what ever is comparable
     int capacity = 0;
     int heapSize = 0;
+    Comparator<TDataType> comparator;
 
-    minHeap(int capacity) throws Exception {
+    public MinHeap(int capacity, Comparator<TDataType> comparator) throws Exception {
         if (capacity <= 0) {
             throw new Exception("Capacity cannot be less than 1");
-
         }
-        array = new int[capacity];
+        array = new Object[capacity]; // T[] ts = (T[]) new Object[n];
         this.capacity = capacity;
-
+        this.comparator = comparator;
     }
 
-    // get the parent
+    // get the parent, 
     public int parent(int index) {
         return (index - 1) / 2;
     }
@@ -36,13 +38,13 @@ public class minHeap {
     // Now, we have 2 main operations on the heaps.
     // 1. Insertion--> WE always insert the new element in the heap at the last
     // index (Bottom right most).
-    public void insert(int index) throws Exception {
+    public void insert(TDataType element) throws Exception {
         // we can have excception here also, if the capacity of the array is full
         if (heapSize > capacity) {
             throw new Exception("No capacity to insert element in the heap");
         }
-        // we inset the element at the last index
-        array[heapSize] = index; // heapsize has now 1 element
+        // we insert the element at the last index
+        array[heapSize] = element; // heapsize has now 1 element
         // once we insert the new element in the heap, it can voilate the heap property
         // , now to maintain that we do the swapping of the parent index and the new
         // index value
@@ -52,51 +54,51 @@ public class minHeap {
     }
 
     public void display() {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i <heapSize ; i++) {
             System.out.println(array[i]);
-
         }
     }
 
     // now to remove the element in the heap
-    public int remove() throws Exception {
+    public TDataType remove() throws Exception {
         if (heapSize == 0) {
             throw new Exception("Heap array is empty, cannot remove the element");
         }
         // while removing the element from the heap, we first swap the root node and the
         // last node of the heap
-        int popped = array[0];
+        Object popped = array[0];
         // swap the root node and the last node
         swap(0, heapSize - 1);
-        array[heapSize] -= 1;
         heapSize -= 1;
         // once we remove the element from the heap, the property of the heap can be
         // voilated, and to counter that we do the topdown approach/ bubbleDown
         bubbleDown(0);
-        return popped;
+        return (TDataType) popped; // Casting
     }
 
     public void bubbleDown(int index) {
-        if (index > heapSize) {
-            return;
-        }
-
         // we need the left and right child and the min index
         int leftchildIndex = leftChild(index);
         int rightchildIndex = rightChild(index);
         int minIndex = index;
-        if (leftchildIndex < array[heapSize] && leftchildIndex < minIndex) {
-            minIndex = leftchildIndex;
+
+        //new
+        if(leftchildIndex < heapSize){
+            int left_cmp = this.comparator.compare((TDataType)array[leftchildIndex], (TDataType)array[index]);
+            if(left_cmp <= -1)
+                minIndex = leftchildIndex;
         }
-        // for the right childindex to be minimum, we need to have the left child,
-        // minIndex also smaller
-        if (rightchildIndex < array[heapSize] && rightchildIndex < minIndex && array[rightchildIndex] < array[leftchildIndex]) {
-            minIndex = rightchildIndex;
+
+        if(rightchildIndex < heapSize){
+            int right_cmp = this.comparator.compare((TDataType)array[rightchildIndex], (TDataType)array[index]);
+            int right_left_cmp = this.comparator.compare((TDataType)array[rightchildIndex], (TDataType)array[leftchildIndex]);
+            if( right_cmp <= -1 && right_left_cmp <= -1)
+                minIndex = rightchildIndex;
         }
+
         // now if the condition is not voilate while removing the element then
         if (minIndex == index) {
             return;
-
         }
         swap(index, minIndex);
         // also again bubble-down once the new indexes are changes
@@ -105,16 +107,15 @@ public class minHeap {
     }
 
     public void bubbleUp(int index) {
-        while (index != 0 && array[parent(index)] > array[index]) {
+        while (index != 0 && this.comparator.compare((TDataType)array[parent(index)],(TDataType)array[index]) >= 1) {
             swap(index, parent(index));
             // once the elements are swapped, indexes change
             index = parent(index);
-
         }
     }
 
     void swap(int first, int second) {
-        int temp = array[first];
+        Object temp = array[first];
         array[first] = array[second];
         array[second] = temp;
     }
